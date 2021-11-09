f6892c4ddf51755f266cd8b013e264206e81f879
ainame <s.namai.09@gmail.com>
2021-11-09 01:56:16 +0000

    Implement step.sh

--- step.sh
+++ step.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -ex
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
+if [ -f .ruby-version ]; then
+    ruby_version_file=$(cat .ruby-version)
+fi
+version="${ruby_version:-${ruby_version_file}}"
+rbenv install --skip-existing "${version}"
 
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
+installed_dir="$(rbenv root)/versions/${version}"
 
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
+# The way adding break lines looks odd here but this is the way we need to follow...
+if [ "${BITRISE_CACHE_INCLUDE_PATHS}" == "" ]; then
+    cache_dir="
+${installed_dir}"
+else
+    # BITRISE_CACHE_INCLUDE_PATHS typically includes a break line at the end
+    cache_dir="${BITRISE_CACHE_INCLUDE_PATHS}
+${installed_dir}"
+fi
+
+envman add --key BITRISE_CACHE_INCLUDE_PATHS --value "${cache_dir}"

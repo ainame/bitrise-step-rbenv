26920d765a417d7b59f433df581eaeef5caecc48
ainame <s.namai.09@gmail.com>
2021-11-11 11:54:00 +0000

    Always try to install rbenv

--- step.sh
+++ step.sh
@@ -8,12 +8,10 @@ function setup_rbenv {
             brew outdated ruby-build || brew upgrade ruby-build
             ;;
         Linux*)
-            if ! rbenv -v 2>&1 > /dev/null; then
-                # https://github.com/rbenv/rbenv-installer
-                curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
-                PATH=${rbenv_root}/bin:$PATH
-                eval $(rbenv init -)
-            fi
+            # https://github.com/rbenv/rbenv-installer
+            curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
+            PATH=${rbenv_root}/bin:$PATH
+            eval $(rbenv init -)
             ;;
         *)
             echo "ERROR: Unknown platform found ${platform}"

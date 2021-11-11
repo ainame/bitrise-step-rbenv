04195bf2ff2532add17e6dac6c4efc535fabaa70
ainame <s.namai.09@gmail.com>
2021-11-11 11:46:08 +0000

    Support rbenv installation in Ubuntu

--- step.sh
+++ step.sh
@@ -1,10 +1,38 @@
 #!/bin/bash
 set -ex
 
+function setup_rbenv {
+    platform="$(uname -s)"
+    case "${platform}" in
+        Darwin*) # macOS
+            brew outdated ruby-build || brew upgrade ruby-build
+            ;;
+        Linux*)
+            if ! rbenv -v 2>&1 > /dev/null; then
+                # https://github.com/rbenv/rbenv-installer
+                curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
+                PATH=${rbenv_root}/bin:$PATH
+                eval $(rbenv init -)
+            fi
+            ;;
+        *)
+            echo "ERROR: Unknown platform found ${platform}"
+            ;;
+    esac
+    curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-doctor | bash
+}
+
+setup_rbenv
+
+# Read .ruby-version
 if [ -f .ruby-version ]; then
     ruby_version_file=$(cat .ruby-version)
 fi
+
+# Determine Ruby version to be installed
 version="${ruby_version:-${ruby_version_file}}"
+
+# Install Ruby if not installed yet
 rbenv install --skip-existing "${version}"
 
 installed_dir="$(rbenv root)/versions/${version}"
@@ -14,7 +42,6 @@ if [ "${BITRISE_CACHE_INCLUDE_PATHS}" == "" ]; then
     cache_dir="
 ${installed_dir}"
 else
-    # BITRISE_CACHE_INCLUDE_PATHS typically includes a break line at the end
     cache_dir="${BITRISE_CACHE_INCLUDE_PATHS}
 ${installed_dir}"
 fi

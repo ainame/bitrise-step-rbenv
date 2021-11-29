17502ad7b96c5ca8242ba357299e6f5941d7e7a1
ainame <s.namai.09@gmail.com>
2021-11-29 11:32:20 +0000

    Append rbenv init config

--- step.sh
+++ step.sh
@@ -17,8 +17,8 @@ function setup_rbenv {
             wget -q "https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-doctor" -O- | bash
 
             # Store rbenv init settings for other steps
-            ~/.rbenv/bin/rbenv init bash 2> ~/.bashrc || echo "Set up ~/.bashrc for rbenv"
-            ~/.rbenv/bin/rbenv init zsh 2> ~/.zshrc || echo "Set up ~/.zshrc for rbenv"
+            ~/.rbenv/bin/rbenv init bash 2>> ~/.bashrc || echo "Set up ~/.bashrc for rbenv"
+            ~/.rbenv/bin/rbenv init zsh 2>> ~/.zshrc || echo "Set up ~/.zshrc for rbenv"
             ;;
         *)
             echo "ERROR: Unknown platform found ${platform}"
